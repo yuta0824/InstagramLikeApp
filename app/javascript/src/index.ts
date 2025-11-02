27b3195a69e7889cb9388cd2b8c2058ad9915cb8
yuta <yu.code.tech@gmail.com>
2025-11-02 22:50:38 +0900

    chore: typescript setup

---
+++ app/javascript/src/index.ts
@@ -0,0 +1,5 @@
+import { test } from "./modules/test";
+
+document.addEventListener("turbo:load", async () => {
+  test();
+});
---
+++ app/javascript/src/modules/test.ts
@@ -0,0 +1,5 @@
+// TODO: テスト後に削除
+
+export const test = () => {
+  alert("test");
+};

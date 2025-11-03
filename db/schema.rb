da196afb794fa8707b2fc37a2be55564c3e033d2
yuta <yu.code.tech@gmail.com>
2025-11-03 14:15:39 +0900

    fix: user テーブルの name を必須に変更

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_03_024529) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

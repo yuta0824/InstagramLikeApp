2e419ec3028488bf28bb47fa896055dcfa513d5c
yuta <yu.code.tech@gmail.com>
2025-11-03 22:27:32 +0900

    chore: Schema Information 更新

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,18 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
+#  name                   :string           not null
 #  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
-#  name                   :string           not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_name                  (name) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 require 'rails_helper'
 

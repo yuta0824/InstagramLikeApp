58dff24fdc7b104050be94a7dff95a98dd235f92
yuta <yu.code.tech@gmail.com>
2025-11-03 14:25:13 +0900

    fix: migration indentation

--- db/migrate/20251103024529_add_name_to_users.rb
+++ db/migrate/20251103024529_add_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameToUsers < ActiveRecord::Migration[7.2]
   def change
-      add_column :users, :name, :string, null: false
+    add_column :users, :name, :string, null: false
     add_index :users, :name, unique: true
   end
 end

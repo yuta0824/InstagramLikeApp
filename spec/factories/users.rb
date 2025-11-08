751ea7d0a260328a27416bc09ed7b78410a846ab
yuta <yu.code.tech@gmail.com>
2025-11-09 00:55:25 +0900

    fix: factories のユーザー名を20文字に切り詰める

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -22,6 +22,6 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
     password { Faker::Internet.unique.password }
-    name { Faker::Name.unique.name }
+    name { Faker::Name.unique.name[0...20] }
   end
 end

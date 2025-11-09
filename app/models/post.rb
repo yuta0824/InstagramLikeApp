e384d0d4b413f522e6ab7d7aca0ffc51b24c4b5e
yuta <yu.code.tech@gmail.com>
2025-11-09 16:33:28 +0900

    feat: Postモデルの実装

---
+++ app/models/post.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  caption    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many_attached :images
+  validates :images, presence: true, length: { minimum: 1, maximum: 3 }
+  validates :caption, length: { maximum: 100 }
+end

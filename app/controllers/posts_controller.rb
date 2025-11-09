00dd4502502a1a962bc41c40e1bf980ef249053d
yuta <yu.code.tech@gmail.com>
2025-11-09 13:33:25 +0900

    feat: 投稿画面の view 実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @user = current_user
+  end
+
+  def new
+    @user = current_user
+    # @post = current_user.posts.build
   end
 end

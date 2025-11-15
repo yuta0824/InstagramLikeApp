476a8afb18cbdbc1a1a690adde845c2cf68b5bb4
yuta <yu.code.tech@gmail.com>
2025-11-15 15:19:29 +0900

    test: system spec 実装

---
+++ spec/system/likes_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Like button', type: :system do
+  let(:post_owner) { create(:user) }
+  let!(:post) { create(:post, user: post_owner) }
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  it 'いいねボタンクリック時に data-liked 属性が切り替わる' do
+    visit root_path
+
+    selector = "[data-post-id='#{post.id}'] .js-like-button"
+    button = find(selector)
+    expect(button[:'data-liked']).to be_nil
+
+    button.click
+    expect(page).to have_css("#{selector}[data-liked='true']")
+
+    find("#{selector}[data-liked='true']").click
+    expect(page).to have_css("#{selector}:not([data-liked])")
+  end
+end

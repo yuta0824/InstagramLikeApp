6bb5d4e57c03c66924116a86a3f6e401b5f5a6d6
yuta <yu.code.tech@gmail.com>
2025-11-15 14:47:28 +0900

    refactor: bin/lint の警告修正

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Like, type: :model do
   let(:post) { create(:post) }
   let(:user) { create(:user) }
 
-  context "user と post の組み合わせが一意の場合" do
+  context 'user と post の組み合わせが一意の場合' do
     it '保存できる' do
       expect(build(:like, user: user, post: post)).to be_valid
     end
   end
 
-  context "user と post の組み合わせが一意ではない場合" do
+  context 'user と post の組み合わせが一意ではない場合' do
     it '保存できない' do
       create(:like, user: user, post: post)
       duplicate = build(:like, user: user, post: post)

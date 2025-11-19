19edc6ef0664e03adb662d9ec21a8ca12d72b6f5
yuta <yu.code.tech@gmail.com>
2025-11-20 00:50:58 +0900

    refactor: n+1問題の対応

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def index
     post = Post.find(params[:post_id])
-    @comments = post.comments
+    @comments = post.comments.includes(:user)
   end
 end

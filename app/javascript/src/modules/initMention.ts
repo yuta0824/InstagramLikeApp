c274d4d356e82e5dfb77ab1a6efea9fe829b4850
yuta <yu.code.tech@gmail.com>
2025-11-20 08:50:02 +0900

    feat: サジェストにアイコン表示

--- app/javascript/src/modules/initMention.ts
+++ app/javascript/src/modules/initMention.ts
@@ -5,13 +5,32 @@ export const initMention = () => {
     document.querySelector<HTMLTextAreaElement>("#js-mention-input");
   if (!commentTextareas) return;
 
+  initTribute(commentTextareas);
+};
+
+const initTribute = (commentTextareas) => {
   const tribute = new Tribute({
     // TODO: APIでユーザー情報を取得する
     values: [
-      { key: "@Phil Heartman", value: "pheartman" },
-      { key: "@Gordon Ramsey", value: "gramsey" },
+      {
+        key: "Phil Heartman",
+        value: "PhilHeartman",
+        avatar: "/assets/icon_avatar1.webp",
+      },
+      {
+        key: "Gordon Ramsey",
+        value: "GordonRamsey",
+        avatar: "/assets/icon_avatar2.webp",
+      },
     ],
+    menuItemTemplate: function (item: any) {
+      return `
+      <div class="flex items-center gap-2 p-2">
+        <img src="${item.original.avatar}" class="size-6 rounded-full" alt="">
+        <span>${item.string}</span>
+      </div>
+    `;
+    },
   });
-
   tribute.attach(commentTextareas);
 };

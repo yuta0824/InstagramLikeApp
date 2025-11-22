517cb2d58e416807f774e080a38fdc61efa92df2
Yuta | CodeYu <yu.code.tech@gmail.com>
2025-11-22 11:48:07 +0900

    feat: add username format validation (#34)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_19_145226) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_22_112059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_19_145226) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.check_constraint "name::text ~ '^[a-zA-Z0-9_]+$'::text", name: "users_name_format"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"

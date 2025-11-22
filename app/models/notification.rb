301dc7951bb4ca2edcebac04762eafa22493daef
yuta <yu.code.tech@gmail.com>
2025-11-22 13:52:45 +0900

    feat: Notificationモデルを追加

---
+++ app/models/notification.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  comment_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_notifications_on_comment_id              (comment_id)
+#  index_notifications_on_comment_id_and_user_id  (comment_id,user_id) UNIQUE
+#  index_notifications_on_user_id                 (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (comment_id => comments.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Notification < ApplicationRecord
+  belongs_to :user
+  belongs_to :comment
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_22_112059) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_22_112060) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_22_112059) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "comment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id", "user_id"], name: "index_notifications_on_comment_id_and_user_id", unique: true
+    t.index ["comment_id"], name: "index_notifications_on_comment_id"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.text "caption"
@@ -91,5 +101,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_22_112059) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "notifications", "comments"
+  add_foreign_key "notifications", "users"
   add_foreign_key "posts", "users"
 end

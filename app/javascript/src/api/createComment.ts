5bba9b64975604612dee201788f234f6b1238a3a
yuta <yu.code.tech@gmail.com>
2025-11-23 16:32:49 +0900

    test: コメントAPIのrswagテストを追加しOpenAPIから型定義を生成

--- app/javascript/src/api/createComment.ts
+++ app/javascript/src/api/createComment.ts
@@ -1,16 +1,23 @@
+import { CommentRequestBody, CommentResponse } from "../types/api";
 import { getCsrfToken } from "../utils/getCsrfToken";
 
-export const createComment = async (postId: string, content: string) => {
-  const csrfToken = getCsrfToken();
-  const requestOptions: RequestInit = {
+export const createComment = async (
+  postId: string,
+  content: string
+): Promise<CommentResponse> => {
+  const requestBody: CommentRequestBody = {
+    comment: { content },
+  };
+
+  const response = await fetch(`/api/posts/${postId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-CSRF-Token": csrfToken,
+      "X-CSRF-Token": getCsrfToken(),
     },
-    body: JSON.stringify({ comment: { content: content } }),
-  };
-  const response = await fetch(`/api/posts/${postId}/comment`, requestOptions);
+    body: JSON.stringify(requestBody),
+  });
+
   if (!response.ok) {
     throw new Error(`レスポンスステータス: (${response.status})`);
   }

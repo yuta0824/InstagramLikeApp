f995cd657c765aaff4c23e8d0955cb8a5ae24250
yuta <yu.code.tech@gmail.com>
2025-11-26 23:40:32 +0900

    検索機能を削除

--- app/controllers/explores_controller.rb
+++ app/controllers/explores_controller.rb
@@ -1,11 +1,6 @@
 class ExploresController < ApplicationController
   def show
-    @users = User.where.not(id: current_user.id).includes(:posts)
-
-    if params[:query].present?
-      @users = @users.where('name LIKE ?', "%#{params[:query]}%")
-    end
-
-    @users = @users.order(Arel.sql('RANDOM()')).limit(50)
+    users = User.where.not(id: current_user.id).includes(:posts)
+    @users = users.order(Arel.sql('RANDOM()')).limit(50)
   end
 end

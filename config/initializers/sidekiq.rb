36592f628000030adcc095a08d9ca4360adeefcc
yuta <yu.code.tech@gmail.com>
2025-11-30 22:11:32 +0900

    fix: seedが本番環境で実行できない問題を解消

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,22 @@
+require 'openssl'
+
+redis_url = ENV['REDIS_TLS_URL']
+redis_url = ENV['REDIS_URL'] if redis_url.nil? || redis_url.empty?
+redis_url ||= 'redis://localhost:6380'
+
+redis_config = { url: redis_url }
+
+if redis_url.start_with?('rediss://')
+  ca_file = ENV['REDIS_SSL_CA_FILE'] || ENV['HEROKU_REDIS_CA_BUNDLE']
+  ssl_params = { verify_mode: OpenSSL::SSL::VERIFY_PEER }
+  ssl_params[:ca_file] = ca_file if ca_file && !ca_file.empty? && File.exist?(ca_file)
+  redis_config[:ssl_params] = ssl_params
+end
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6380') }
+  config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6380') }
+  config.redis = redis_config
 end

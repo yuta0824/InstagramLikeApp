ac87d9ed48bd268e841291af64efd96b27678dd3
yuta <yu.code.tech@gmail.com>
2025-11-30 20:38:38 +0900

    chore: 本番環境ではDotenvでは読み込まない

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ module InstagramLikeApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

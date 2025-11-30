c9c57754575043d4d84581cf4ba7ba66a12d423a
yuta <yu.code.tech@gmail.com>
2025-11-30 22:27:45 +0900

    fix: SidekiqにおけるRedis TLS CA検出を適切に処理する

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,18 @@
 require 'openssl'
 
-redis_url = ENV['REDIS_TLS_URL']
-redis_url = ENV['REDIS_URL'] if redis_url.nil? || redis_url.empty?
-redis_url ||= 'redis://localhost:6380'
-
+redis_url = ENV['REDIS_TLS_URL'].presence || ENV['REDIS_URL'].presence || 'redis://localhost:6380'
 redis_config = { url: redis_url }
 
 if redis_url.start_with?('rediss://')
-  ca_file = ENV['REDIS_SSL_CA_FILE'] || ENV['HEROKU_REDIS_CA_BUNDLE']
-  ssl_params = { verify_mode: OpenSSL::SSL::VERIFY_PEER }
-  ssl_params[:ca_file] = ca_file if ca_file && !ca_file.empty? && File.exist?(ca_file)
-  redis_config[:ssl_params] = ssl_params
+  ca_file = ENV['REDIS_SSL_CA_FILE'].presence || ENV['HEROKU_REDIS_CA_BUNDLE'].presence
+
+  redis_config[:ssl_params] =
+    if ca_file && File.exist?(ca_file)
+      { verify_mode: OpenSSL::SSL::VERIFY_PEER, ca_file: ca_file }
+    else
+      Rails.logger.warn('[Sidekiq] Redis TLS CA bundle not found; disabling certificate verification') if defined?(Rails)
+      { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    end
 end
 
 Sidekiq.configure_server do |config|

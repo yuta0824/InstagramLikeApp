4aa8b0912f705cd0d421174424a1af9e51e27a44
yuta <yu.code.tech@gmail.com>
2025-11-30 13:30:13 +0900

    fix(i18n): 言語切り替え時にリロード不要に修正

--- app/javascript/src/utils/i18n.ts
+++ app/javascript/src/utils/i18n.ts
@@ -2,9 +2,18 @@ import { I18n } from "i18n-js";
 import translations from "../../locales.json";
 
 const i18n = new I18n(translations);
-const locale =
-  document.body.dataset.locale || navigator.language.split("-")[0] || "en";
-i18n.locale = locale;
+
+Object.defineProperty(i18n, "locale", {
+  get() {
+    return (
+      document.body.dataset.locale || navigator.language.split("-")[0] || "en"
+    );
+  },
+  set(value) {
+    document.body.dataset.locale = value;
+  },
+});
+
 i18n.defaultLocale = "en";
 i18n.enableFallback = true;
 

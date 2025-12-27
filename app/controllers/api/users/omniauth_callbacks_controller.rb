f1ba51f605aba5a2ca5952c5419def0d0f357019
yuta <yu.code.tech@gmail.com>
2025-12-27 11:57:13 +0900

    feat: コールバックURLを変更

--- app/controllers/api/users/omniauth_callbacks_controller.rb
+++ app/controllers/api/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,9 @@ class Api::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
     user = User.from_omniauth(request.env['omniauth.auth'])
     auth_code = SecureRandom.hex(16)
     Rails.application.config.x.redis.setex("auth_code:#{auth_code}", 60, user.id)
-    redirect_to "#{ENV.fetch('FRONTEND_URL')}/auth/callback?auth_code=#{auth_code}", allow_other_host: true
+    redirect_to "#{ENV.fetch('FRONTEND_URL')}/auth-callback?auth_code=#{auth_code}", allow_other_host: true
     rescue StandardError
-      redirect_to "#{ENV.fetch('FRONTEND_URL')}/auth/failure", allow_other_host: true
+      redirect_to "#{ENV.fetch('FRONTEND_URL')}/?auth_error=google", allow_other_host: true
   end
 end
+

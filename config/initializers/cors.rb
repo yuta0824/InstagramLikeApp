0012d19e3d81c7041086b3977bdff0ebff647584
yuta <yu.code.tech@gmail.com>
2025-12-30 20:39:34 +0900

    fix: FRONTEND_URL fallback

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,16 @@
+origin =
+  if Rails.env.production?
+    ENV.fetch('FRONTEND_URL')
+  else
+    ENV.fetch('FRONTEND_URL', 'http://localhost:3000')
+  end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('FRONTEND_URL')
+    origins origin
     resource '*',
-      headers: :any,
-      methods: %i[get post put patch delete options head],
-      expose: %w[Authorization]
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose: %w[Authorization]
   end
 end

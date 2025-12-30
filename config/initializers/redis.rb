f4d01bd906f41989d7db7838e012f133106bd272
yuta <yu.code.tech@gmail.com>
2025-12-30 20:36:04 +0900

    fix: REDIS_URL fallback

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,8 @@
-Rails.application.config.x.redis = Redis.new(url: ENV.fetch('REDIS_URL'))
+url =
+  if Rails.env.production?
+    ENV.fetch('REDIS_URL')
+  else
+    ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
+  end
+
+Rails.application.config.x.redis = Redis.new(url:)

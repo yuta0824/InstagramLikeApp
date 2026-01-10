eae3e337adde8efe68c916b01efc164c064551d8
yuta <yu.code.tech@gmail.com>
2026-01-10 23:10:42 +0900

    feat: post update

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,10 +1,6 @@
 class Api::PostsController < ApplicationController
   def show
-    post = Post
-             .with_associations
-             .includes(comments: [user: { avatar_attachment: :blob }])
-             .find(params[:id])
-
+    post = find_post(params[:id])
     render json: post, serializer: PostDetailSerializer, scope: current_user, status: :ok
   end
 
@@ -18,9 +14,27 @@ class Api::PostsController < ApplicationController
     end
   end
 
+  def update
+    post = current_user.posts.find(params[:id])
+
+    if post.update(post_params)
+      updated_post = find_post(post.id)
+      render json: updated_post, serializer: PostDetailSerializer, scope: current_user, status: :ok
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
     params.fetch(:post, params).permit(:caption, images: [])
+  end
+
+  def find_post(id)
+    Post
+      .with_associations
+      .includes(comments: [user: { avatar_attachment: :blob }])
+      .find(id)
   end
 end
